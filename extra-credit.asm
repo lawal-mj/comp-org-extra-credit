41eecd7f8700c1e0799f7c2c5e42757b9ced9780
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2023-12-04 18:46:53 -0500

    create data segment

---
+++ extra-credit.asm
@@ -0,0 +1,8 @@
+.data
+promptL: .asciiz "Enter a positive integer L greater than 20: "
+promptM: .asciiz "Enter a positive integer M greater than 20: "
+promptN: .asciiz "Enter a positive integer N greater than 20: "
+resultMsg: .asciiz "The Greatest Common Divisor is: "
+
+.text
+.globl main
